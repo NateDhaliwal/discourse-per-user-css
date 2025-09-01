79ffed177b878e51e6fa4b8c77b6c9586c477f7f
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-02 07:53:15 +0800

    Update todo.js

--- javascripts/discourse/api-initializers/todo.js
+++ javascripts/discourse/api-initializers/todo.js
@@ -1,5 +1,13 @@
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer((/* api */) => {
-  // Your code here (uncomment api above to use it)
+  api.addSaveableUserOptionField({
+    key: "user_css_edit",      // the custom user field name
+    default: "",                // default value (empty string for code)
+    type: "string",             // type string for code or queries
+    label: "Custom CSS code",        // label in the preferences UI
+    tab: "interface",             // which preferences tab to show under
+    // Optionally add a custom "inputComponent" (for custom UI, see below)
+    inputComponent: "form-kit/code"  // If you want a code editor
+  });
 });
