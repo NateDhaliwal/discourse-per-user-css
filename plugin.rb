5e466abe846f6a0e45892d8e6c17b869cf6dde73
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-09 10:40:41 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,5 +5,9 @@
 # url: https://github.com/NateDhaliwal/discourse-per-user-css
 
 register_editable_user_custom_field :user_css_code
-User.register_custom_field_type 'user_css_code', :string
 DiscoursePluginRegistry.serialized_current_user_fields << 'user_css_code'
+
+after_initialize do
+  User.register_custom_field_type('user_css_code', :string)
+  add_to_serializer(:user, :user_css_code) { object.user_css_code }
+end
